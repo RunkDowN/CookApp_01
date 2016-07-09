97eefd49379885e9d6cadac1d0a5ea2057bd6d1d
RunkDowN <runkdown@gmail.com>
2016-07-10 01:57:41 +0300

    Extending login fragment

--- app/src/main/java/com/inc/runkdown/cookapp_01/LoginFragment.java
+++ app/src/main/java/com/inc/runkdown/cookapp_01/LoginFragment.java
@@ -17,7 +17,7 @@ import android.widget.TextView;
  * Created by RunkDowN on 30.06.16.
  */
 public class LoginFragment extends android.support.v4.app.Fragment {
-    EditText UsernameEt;
+//    EditText UsernameEt;
     TextView Login, OnRegLabel;
 
     public LoginFragment(){
@@ -27,16 +27,13 @@ public class LoginFragment extends android.support.v4.app.Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        UsernameEt = (EditText) getView().findViewById(R.id.et_email);
     }
 
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.login_fragment, container, false);
 
-
-
-        UsernameEt = (EditText) rootView.findViewById(R.id.et_email);
+        EditText UsernameEt = (EditText) rootView.findViewById(R.id.et_email_login);
         UsernameEt.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -50,13 +47,14 @@ public class LoginFragment extends android.support.v4.app.Fragment {
 
             @Override
             public void afterTextChanged(Editable s) {
+
                 System.out.println("LISTENER AFTERTEXT WORKING");
                 System.out.println("==================================");
                 Login = (TextView) getView().findViewById(R.id.loginText);
                 Login.setTextColor(Color.argb(255, 255, 255, 255));
             }
         });
-        return inflater.inflate(R.layout.login_fragment, container, false);
+        return rootView;
     }
 }
 
